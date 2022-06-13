78e419efb09b346f068b2aa682dde7c876ec27f2
Anton Matiunin <anton.matiunin@onix-systems.com>
2022-06-13 15:17:57 +0300

    adding some tests and fixes

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -2,17 +2,15 @@ require 'digest/sha2'
 
 class Shortener
 
-  BASE_URL = "http://domain.com/"
-
   attr_reader :url
 
   def initialize(url)
     @url = url
   end
 
-  def self.shorts_url(url)
+  def self.shorts_url(url, application_host)
     code = Digest::SHA256.hexdigest(url)[0..4]
-    short_url = BASE_URL + code
+    short_url = application_host +"/"+ code
     Url.create(full_url: url, short_url: short_url) unless Url.where(full_url: url).present?
     short_url
   end
